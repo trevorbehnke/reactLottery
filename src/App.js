8edb0e4f863431cf2efa89aab5e3e73eca8e3ca0
trevor <me@trevorbehnke.com>
2021-08-08 21:48:15 -0700

    + logic, styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-import Ball from "./Ball";
+import Lottery from "./Lottery";
 
-function App() {
-  return (
-    <div className="App">
-      <Ball num={17} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
+      </div>
+    );
+  }
 }
 
 export default App;
